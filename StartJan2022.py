428929db4ec3aaa626b04fa3de6b4aef1439c86b
Hossein Darvish <65111057+HDarvish@users.noreply.github.com>
2022-01-06 13:57:07 +0100

    Add files via upload

---
+++ StartJan2022.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  6 11:35:37 2022
+
+@author: SMEA
+"""
+
+# Here we are going to remodel the paper by Boston et al. 2021
+
+# in the section 2.5 we can find the flowchart and also the main code behind 
+# such a system!
+
+# this is a paragraph form the method of this paper:
+    
+    
+    # The hourly generated PV power is taken as input to the model,
+    # see Section 2.4. The energy load, besides road going transport,
+    # of Spain is discussed and calculated in Section 2.1.
+    # The hourly energy load, besides road going transport,
+    # is calculated and used as input to the simulation model 
+    # according to the profiles shown in Section 2.3. Lastly, 
+    # the hourly energy load of the road going transport is calculated 
+    # according to Section 2.2 and subsequently given as an input to 
+    # the simulation model. The simulation models calculates 
+    # the state of the charge (SOC) for the aggregated EV fleet with 
+    # an hourly temporal resolution. Movingvehiclesand stationary vehicles
+    # are treated separately in the calculation.
